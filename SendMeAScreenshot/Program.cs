12e367d1843d071c514a699c5c60ab583b694b9b
KarlTornblom <55096906+KarlTornblom@users.noreply.github.com>
2019-11-07 17:11:32 +0100

    Screenshot saved

--- SendMeAScreenshot/Program.cs
+++ SendMeAScreenshot/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net.Mail;
 
-namespace SendMeAScreenshot
+namespace RemoteFamilyITSupport
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Bitmap memoryImage;
+            memoryImage = new Bitmap(Convert.ToInt32(Screen.PrimaryScreen.Bounds.Width), Convert.ToInt32(Screen.PrimaryScreen.Bounds.Height));
+            Size s = new Size(memoryImage.Width, memoryImage.Height);
+
+            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+
+            memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+            string str = "";
+            try
+            {
+                str = string.Format("Screenshot.png");
+            }
+            catch (Exception er)
+            {
+                Console.WriteLine("Error occured: " + er.Message);
+                Console.ReadKey();
+            }
+
+            memoryImage.Save(str);
+
+            SendMail();
+        }
+
+        private static void SendMail()
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            mail.From = new MailAddress("");
+            mail.To.Add = ("");
         }
     }
+
 }
